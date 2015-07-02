71d02e22edc3ac7bbaefa83619f07408aeb23893
Geodracs <geodracs@gmail.com>
2015-07-02 10:45:28 +0200

    add routes

---
+++ app/app.routes.js
@@ -0,0 +1,13 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/login");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            templateUrl: "templates/login.html"
+        })
+
+});
