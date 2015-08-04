6eb8fcb543d9e174297b414cd8fce065fb450080
Salvador <geodracs@gmail.com>
2015-08-04 12:01:43 +0200

    init

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var app = angular.module("vts", ['ui.router']);
 
 
 app.controller("playlist", function($scope){
+
     $scope.list = [
         {id:1,title:"Introduccion"},
         {id:1,title:"Variables"},
