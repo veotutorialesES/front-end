78e0d7e659d3ba2dda1df1dea7c303a83743bdd7
geodracs <geodracs@gmail.com>
2015-10-11 00:31:41 +0200

    wi

--- assets/js/app.js
+++ assets/js/app.js
@@ -467,31 +467,12 @@ app.controller("viewController", function($scope,$api,$state){
     };
 
 });;
-app.controller("accountController", function($scope,$subscription,$api,$state,$rootScope){
-    $scope.subscriptionItems = [];
-
-    $scope.subscriptions = function(type) {
-        console.info("subscriptionController: subscriptions()")
-
-        $subscription.list(type, function (res) {
-            console.info("subscriptionController->subscriptions()")
-            console.log(res);
-            $scope.subscriptionItems = res;
-        })
-    };
+app.controller("accountController", function($scope,$api,$state,$rootScope){
 
 
-    $scope.unsubscribe = function(type, type_id){
-        console.info("subscriptionController: unsubscribe("+type+","+type_id+")");
-
-        $subscription.delete(type,type_id, function(res){
-            console.info("subscriptionController->unsubscribe()")
-            console.log(res);
-            $scope.subscriptions(type); // TODO eliminar manualmente del array
-        });
-    };
 
 
+ 
     $scope.updateName = function(name,pass){
 
         var arr = [];
